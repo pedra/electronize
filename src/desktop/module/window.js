6c0be8ce91438089e476510f272f23585cf152ce
pedra <prbr@ymail.com>
2021-05-20 00:35:33 -0300

    refactoring window

--- src/desktop/module/window.js
+++ src/desktop/module/window.js
@@ -8,43 +8,20 @@
  */
 
 const path = require('path')
-const fs = require('fs')
 const { app, BrowserWindow } = require('electron')
 
 module.exports = function () {
 
-    /* Instancia da janela criada.
-
-        TODO: transformar em um array de Windows no formato:
-            Window = [{ 
-                name: 'Main',
-                instance: null // Objeto criado com -> new janela()
-            }, ...]
-     */
-    let Janelas = [],
-        Main = null
-
+    let Janelas = [],   // Window array 
+        Main = null     // Them application main window
 
     /**
      * Create a window or return your previously created instance
      * @returns Object Returns the window
      */
-    const create = window => {
-        if (window == 'main') {
-            if (Main == null || Main.isDestroyed()) {
-                Main = new janela('main')
-                return Main
-            } else {
-                throw new Error('Main window already exists!')
-                process.exit(1)
-            }
-        }
-
-        // Create a window
-        return !Janelas[window] || Janelas[window].isDestroyed() ?
-            (Janelas[window] = new janela(window)) :
-            Janelas[window]
-    }
+    const create = window => !Janelas[window] || Janelas[window].isDestroyed() ?
+        (Janelas[window] = new janela(window)) :
+        Janelas[window]
 
     /**
      * Returns a window 
@@ -57,36 +34,10 @@ module.exports = function () {
     }
 
     /**
-     * Returns an instance of the main application window
-     * @returns Object  The application Main window or null
+     * Creates a window
+     * @param {String} html Name of the window
+     * @returns Object
      */
-    // const getMainInstance = () => Main
-
-
-    // /**
-    //  * Return a previously created instance or create a new window
-    //  * @param {String} html Name of window to
-    //  * @returns Object Return a new window or a instance of previously created window with same name
-    //  */
-    // const getInstanceOrCreate = (html) => {
-    //     if (html == 'main') return false
-    //     return !Janelas[html] || Janelas[html].isDestroyed() ?
-    //         (Janelas[html] = new janela(html)) :
-    //         Janelas[html]
-    // }
-
-
-    /* Cria e/ou retorna a instância do Window (janela principal)
-
-        TODO: criar mais janelas e gravar em um "pool" (array) de Windows.
-           As janelas seriam referenciadas pela chamada a getInstance('nome da janela')          
-    */
-    // const getInstanceOrCreate = (html) =>
-    //     Window == null || Window.isDestroyed() ?
-    //         (Window = new janela(html)) :
-    //         Window
-
-    // Cria uma janela (Window)
     const janela = function (html) {
 
         // novo Browser...
@@ -123,7 +74,7 @@ module.exports = function () {
         //win.webContents.openDevTools()
 
         // Carrega o arquivo HTML da janela.
-        var lf = win.loadFile(path.join(app.Config.desktop.assets, 'html', html + '.html'))
+        win.loadFile(`${app.Config.desktop.assets}/html/${html}.html`)
 
         // Mostra a janela
         win.once('ready-to-show', () => win.show())
@@ -159,7 +110,7 @@ module.exports = function () {
             win.hide()
             app.Config.visible = false
 
-            console.log('\nminimize | trabalhando em background! | Visible: ', app.Config.visible)
+            console.log(`\nminimize | (${html}) trabalhando em background! | Visible: `, app.Config.visible)
         })
 
         // Ao fechar a janela ...
@@ -168,28 +119,28 @@ module.exports = function () {
             win.hide()
             app.Config.visible = false
 
-            console.log('\nclose | trabalhando em background! | Visible: ', app.Config.visible)
+            console.log(`\nclose | (${html}) trabalhando em background! | Visible:`, app.Config.visible)
         })
 
-        // Emitido quando a janela é fechada.
+        // Emitido quando a janela é destruída.
         win.on('closed', e => {
-            console.log('\nclosed | A aplicação foi desligada.')
+            console.log(`\nclosed | A Janela (${html}) foi destruída!`)
         })
 
         // Quando mostrar a janela
         win.on('show', e => {
-            console.log('\nshow | trabalhando com janela! | Visible: ', app.Config.visible)
+            console.log(`\nshow | (${html}) trabalhando com janela! | Visible: `, app.Config.visible)
         })
 
         // Janela obteve foco
         win.on('focus', () => {
-            console.log('\nfocus | A janela obteve foco.')
+            console.log(`\nfocus | A Janela (${html}) obteve foco.`)
             setTimeout(() => win.flashFrame(false), 1500)
         })
 
         // Janela perdeu foco
         win.on('blur', () => {
-            console.log('\nblur | A janela perdeu foco.')
+            console.log(`\nblur | A Janela (${html}) perdeu foco.`)
             if (!win.isDestroyed()) {
                 win.flashFrame(true)
                 setTimeout(() => !win.isDestroyed() ? win.flashFrame(false) : false, 1500)
@@ -200,10 +151,16 @@ module.exports = function () {
     }
 
     // Creates a Main application window
-    app.on('ready', () => create('main'))
+    app.on('ready', () => {
+        if (Main == null || Main.isDestroyed()) {
+            Main = new janela('main')
+        } else {
+            throw new Error('Main window already exists!')
+            process.exit(1)
+        }
+    })
 
     return {
         create, get
     }
-
 }
